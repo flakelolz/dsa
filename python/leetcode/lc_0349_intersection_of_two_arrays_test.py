cd671ecca081d4b8eb6a77314dec2716565ddca5
Michael Gonzalez <michaelgonzalez.code@gmail.com>
2024-06-20 16:53:09 -0400

    Update

---
+++ python/leetcode/lc_0349_intersection_of_two_arrays_test.py
@@ -0,0 +1,31 @@
+from typing import List
+
+
+def intersection(nums1: List[int], nums2: List[int]) -> List[int]:
+    hashset = set(nums1)
+    result = []
+
+    for num in nums2:
+        if num in hashset:
+            result.append(num)
+            hashset.remove(num)
+
+    return result
+
+
+def test_intersection_1():
+    nums1 = [1, 2, 2, 1]
+    nums2 = [2, 2]
+    assert intersection(nums1, nums2) == [2]
+
+
+def test_intersection_2():
+    nums1 = [4, 9, 5]
+    nums2 = [9, 4, 9, 8, 4]
+    assert intersection(nums1, nums2) == [9, 4]
+
+
+if __name__ == "__main__":
+    nums1 = [1, 2, 2, 1]
+    nums2 = [2, 2]
+    intersection(nums1, nums2)
