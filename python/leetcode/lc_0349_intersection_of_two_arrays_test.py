807857bd3f5569c734e90f283bd30a84cd645604
Michael Gonzalez <michaelgonzalez.code@gmail.com>
2024-06-20 16:53:26 -0400

    Update

--- python/leetcode/lc_0349_intersection_of_two_arrays_test.py
+++ python/leetcode/lc_0349_intersection_of_two_arrays_test.py
@@ -23,9 +23,3 @@ def test_intersection_2():
     nums1 = [4, 9, 5]
     nums2 = [9, 4, 9, 8, 4]
     assert intersection(nums1, nums2) == [9, 4]
-
-
-if __name__ == "__main__":
-    nums1 = [1, 2, 2, 1]
-    nums2 = [2, 2]
-    intersection(nums1, nums2)
