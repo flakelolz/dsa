bd9a74e743a48cf09addaf3fde5bd4db5ee71c83
Michael Gonzalez <michaelgonzalez.code@gmail.com>
2024-08-10 00:00:14 -0400

    Update

--- python/leetcode/lc_0206_reverse_linked_list_test.py
+++ python/leetcode/lc_0206_reverse_linked_list_test.py
@@ -16,3 +16,27 @@ def reverseList(head: Optional[ListNode]) -> Optional[ListNode]:
         curr = nxt
 
     return prev
+
+
+def checker(list1, list2):
+    while list1 and list2:
+        if list1.val != list2.val:
+            return False
+        list1 = list1.next
+        list2 = list2.next
+    return True
+
+def test_reverseList_1():
+    list1 = ListNode(1)
+    list1.next = ListNode(2)
+    list1.next.next = ListNode(3)
+    list1.next.next.next = ListNode(4)
+    list1.next.next.next.next = ListNode(5)
+
+    answer = ListNode(5)
+    answer.next = ListNode(4)
+    answer.next.next = ListNode(3)
+    answer.next.next.next = ListNode(2)
+    answer.next.next.next.next = ListNode(1)
+
+    assert checker(reverseList(list1), answer) is True
